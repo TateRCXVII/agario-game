011e7c0cd9e1d1968c0496fd15f71ad1ea47832e
TateRCXVII <tate.reynolds@gmail.com>
2022-04-13 18:33:26 -0600

    started connection gui and implemented a simple draw scene emthod

--- ClientGUI/ClientGUI.Designer.cs
+++ ClientGUI/ClientGUI.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.player_name_label = new System.Windows.Forms.Label();
+            this.server_label = new System.Windows.Forms.Label();
+            this.player_name_box = new System.Windows.Forms.TextBox();
+            this.server_box = new System.Windows.Forms.TextBox();
+            this.error_label = new System.Windows.Forms.Label();
+            this.fps_label = new System.Windows.Forms.Label();
+            this.players_label = new System.Windows.Forms.Label();
+            this.mass_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // player_name_label
+            // 
+            this.player_name_label.AutoSize = true;
+            this.player_name_label.Enabled = false;
+            this.player_name_label.Font = new System.Drawing.Font("Segoe UI Black", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player_name_label.Location = new System.Drawing.Point(696, 491);
+            this.player_name_label.Name = "player_name_label";
+            this.player_name_label.Size = new System.Drawing.Size(253, 50);
+            this.player_name_label.TabIndex = 0;
+            this.player_name_label.Text = "Player Name";
+            this.player_name_label.Visible = false;
+            // 
+            // server_label
+            // 
+            this.server_label.AutoSize = true;
+            this.server_label.Enabled = false;
+            this.server_label.Font = new System.Drawing.Font("Segoe UI Black", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.server_label.Location = new System.Drawing.Point(696, 575);
+            this.server_label.Name = "server_label";
+            this.server_label.Size = new System.Drawing.Size(139, 50);
+            this.server_label.TabIndex = 0;
+            this.server_label.Text = "Server";
+            this.server_label.Visible = false;
+            // 
+            // player_name_box
+            // 
+            this.player_name_box.Enabled = false;
+            this.player_name_box.Font = new System.Drawing.Font("Segoe UI Semibold", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player_name_box.Location = new System.Drawing.Point(1010, 496);
+            this.player_name_box.Name = "player_name_box";
+            this.player_name_box.Size = new System.Drawing.Size(359, 46);
+            this.player_name_box.TabIndex = 1;
+            this.player_name_box.Visible = false;
+            // 
+            // server_box
+            // 
+            this.server_box.Enabled = false;
+            this.server_box.Font = new System.Drawing.Font("Segoe UI Semibold", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.server_box.Location = new System.Drawing.Point(1010, 580);
+            this.server_box.Name = "server_box";
+            this.server_box.Size = new System.Drawing.Size(359, 46);
+            this.server_box.TabIndex = 1;
+            this.server_box.Visible = false;
+            // 
+            // error_label
+            // 
+            this.error_label.AutoSize = true;
+            this.error_label.BackColor = System.Drawing.Color.IndianRed;
+            this.error_label.Enabled = false;
+            this.error_label.Font = new System.Drawing.Font("Segoe UI Black", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.error_label.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.error_label.Location = new System.Drawing.Point(35, 1337);
+            this.error_label.Name = "error_label";
+            this.error_label.Size = new System.Drawing.Size(131, 50);
+            this.error_label.TabIndex = 0;
+            this.error_label.Text = "Errors";
+            this.error_label.Visible = false;
+            // 
+            // fps_label
+            // 
+            this.fps_label.AutoSize = true;
+            this.fps_label.Enabled = false;
+            this.fps_label.Font = new System.Drawing.Font("Segoe UI Black", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.fps_label.Location = new System.Drawing.Point(1983, 33);
+            this.fps_label.Name = "fps_label";
+            this.fps_label.Size = new System.Drawing.Size(65, 37);
+            this.fps_label.TabIndex = 0;
+            this.fps_label.Text = "FPS";
+            this.fps_label.Visible = false;
+            // 
+            // players_label
+            // 
+            this.players_label.AutoSize = true;
+            this.players_label.Enabled = false;
+            this.players_label.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.players_label.Location = new System.Drawing.Point(1927, 70);
+            this.players_label.Name = "players_label";
+            this.players_label.Size = new System.Drawing.Size(114, 37);
+            this.players_label.TabIndex = 0;
+            this.players_label.Text = "Players";
+            this.players_label.Visible = false;
+            // 
+            // mass_label
+            // 
+            this.mass_label.AutoSize = true;
+            this.mass_label.Enabled = false;
+            this.mass_label.Font = new System.Drawing.Font("Segoe UI Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mass_label.Location = new System.Drawing.Point(1960, 118);
+            this.mass_label.Name = "mass_label";
+            this.mass_label.Size = new System.Drawing.Size(85, 37);
+            this.mass_label.TabIndex = 0;
+            this.mass_label.Text = "Mass";
+            this.mass_label.Visible = false;
+            // 
+            // ClientGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(2304, 1413);
+            this.Controls.Add(this.server_box);
+            this.Controls.Add(this.player_name_box);
+            this.Controls.Add(this.server_label);
+            this.Controls.Add(this.error_label);
+            this.Controls.Add(this.mass_label);
+            this.Controls.Add(this.players_label);
+            this.Controls.Add(this.fps_label);
+            this.Controls.Add(this.player_name_label);
+            this.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Name = "ClientGUI";
+            this.Tag = "";
+            this.Text = "Agario Client - CS3500";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label player_name_label;
+        private Label server_label;
+        private TextBox player_name_box;
+        private TextBox server_box;
+        private Label error_label;
+        private Label fps_label;
+        private Label players_label;
+        private Label mass_label;
     }
 }
--- ClientGUI/ClientGUI.cs
+++ ClientGUI/ClientGUI.cs
@@ -1,10 +1,32 @@
+using Communications;
+using System.Diagnostics;
+
 namespace ClientGUI
 {
     public partial class ClientGUI : Form
     {
+        private int count;
+        private double fps;
+        private Stopwatch stopwatch;
+        private Networking network;
+
         public ClientGUI()
         {
+
+            this.Paint += Draw_Scene;
+
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000 / 30;  // 1000 milliseconds in a second divided by 30 frames per second
+            timer.Tick += (a, b) => this.Invalidate();
+
+            timer.Start();
+            stopwatch = Stopwatch.StartNew();
             InitializeComponent();
+        }
+
+        private void Draw_Scene(object sender, PaintEventArgs e)
+        {
+            count++;
         }
     }
 }
